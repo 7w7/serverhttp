372242751513ee2fe7a940e541d5429bc9112bf8
zhouqi <mushroom0523zq@sina.com>
2018-08-02 03:00:46 +0800

    任务5bash脚本

--- demo.sh
+++ demo.sh
@@ -1,29 +1,14 @@
-#!/usr/bin/env node
-
- var fs = require('fs')
-
- var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
-
- fs.readFile(dirName, (err, data) => {
-  if (err)  throw err
- })
-
-fs.mkdirSync("./" + dirName) // mkdir $1
-process.chdir("./" + dirName) // cd $1
-fs.mkdirSync('css') // mkdir css
-fs.mkdirSync('js') // mkdir js
-
-
-fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
-fs.writeFileSync("css/style.css", " h1{color: red;}")
-fs.writeFileSync("./js/main.js", "var string = 'Hello World';alert(string);")
-process.exit(0)
-  
-
-
-    
-
-
-  
- 
-
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo '<!DOCTYPE><title>Hello</title><h1>Hi</h1>' >index.html
+  echo 'h1{color: red;}' > css/style.css
+  echo ' var string = "Hello World" 
+  alert(string)' > js/main.js
+  exit
+fi
